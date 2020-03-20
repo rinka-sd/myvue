78f0bf1318f6354154b2efd70f13b628f8d5b62f
rinka-sd <1152657584@qq.com>
2020-03-20 14:39:07 +0800

    use async/await to simplify login part

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,77 +1,68 @@
 <template>
   <div class="login-wrap">
-    <el-form 
-    class="login-form"
-    label-position="top" 
-    label-width="80px" 
-    :model="formdata">
-    <h2>用户登录</h2>
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+      <h2>用户登录</h2>
       <el-form-item label="用户名">
         <el-input v-model="formdata.username"></el-input>
       </el-form-item>
       <el-form-item label="密码">
         <el-input v-model="formdata.password"></el-input>
       </el-form-item>
-      <el-button 
-      @click.prevent="handleLogin()"
-      class="login-btn" type="primary">登录</el-button>
+      <el-button @click.prevent="handleLogin()" class="login-btn" type="primary">登录</el-button>
     </el-form>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            formdata:{
-                username:'',
-                password:''
-            }
-        }
-    },
-    methods:{
-        // 登录请求
-        handleLogin(){
-            this.$http.post('login',this.formdata)
-            .then((res)=>{
-                //console.log(res);
-                const {
-                    data,
-                    meta:{msg,status}
-                } = res.data
+  data() {
+    return {
+      formdata: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    // 登录请求
+    async handleLogin() {
+      const res = await this.$http.post("login", this.formdata);
 
-                if(status === 200){
-                    //login success
-                    //1.jump to home
-                    this.$router.push({name:'home'})
-                    //2.success notice
-                    this.$message.success(msg)
-                }else{
-                    this.$message.warning(msg)
-                }
-                
-            })
-        }
+      const {
+        data,
+        meta: { msg, status }
+      } = res.data;
+
+      if (status === 200) {
+        //login success
+        //1.jump to home
+        this.$router.push({ name: "home" });
+        //2.success notice
+        this.$message.success(msg);
+      } else {
+        this.$message.warning(msg);
+      }
     }
+  }
 };
 </script>
 
 <style>
-.login-wrap{
-    height: 100%;
-    background: #324152;
-    /* 弹性盒子布局 */
-    display:flex;
-    justify-content: center;
-    align-items: center;
+.login-wrap {
+  height: 100%;
+  background: #324152;
+  /* 弹性盒子布局 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-.login-wrap .login-form{
-    width: 400px;
-    background-color: #fff;
-    border-radius: 5px;
-    padding: 30px;
+.login-wrap .login-form {
+  width: 400px;
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 30px;
 }
-.login-wrap .login-btn{
-    width: 100%;
+.login-wrap .login-btn {
+  width: 100%;
 }
 </style>
