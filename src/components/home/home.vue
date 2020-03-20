fb2fa68778d57188a4358a4024f96028c167c336
rinka-sd <1152657584@qq.com>
2020-03-20 15:30:33 +0800

    add aside base

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -4,7 +4,7 @@
       <el-row>
         <el-col :span="4">
           <div class="grid-content bg-purple">
-              <img class="logo" src="../../assets/img/logo.png" alt="无法显示图片">
+            <img class="logo" src="../../assets/img/logo.png" alt="无法显示图片" />
           </div>
         </el-col>
         <el-col :span="18" class="middle">
@@ -12,23 +12,92 @@
         </el-col>
         <el-col :span="2">
           <div class="grid-content bg-purple">
-              <a class="loginout" href="#">退出</a>
+            <a class="loginout" href="#">退出</a>
           </div>
         </el-col>
       </el-row>
     </el-header>
     <el-container>
-      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-aside class="aside" width="200px">
+        <el-menu
+        :unique-opened="true"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="2-1">
+                <i class="el-icon-menu"></i>
+                <span>角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="2-2">
+                <i class="el-icon-menu"></i>
+                <span>权限管理</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="3-1">
+                <i class="el-icon-menu"></i>
+                <span>商品列表</span>
+            </el-menu-item>
+            <el-menu-item index="3-2">
+                <i class="el-icon-menu"></i>
+                <span>分类参数</span>
+            </el-menu-item>
+            <el-menu-item index="3-3">
+                <i class="el-icon-menu"></i>
+                <span>商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="4-1">
+                <i class="el-icon-menu"></i>
+                <span>订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="5-1">
+                <i class="el-icon-menu"></i>
+                <span>数据报表</span>
+            </el-menu-item>
+          </el-submenu>
+          
+
+        </el-menu>
+      </el-aside>
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
-export default {
-  data() {},
-  methods: {}
-};
 </script>
 
 <style>
@@ -44,15 +113,15 @@ export default {
 .main {
   background-color: #e9eef3;
 }
-.logo{
-    height: 60px;
+.logo {
+  height: 60px;
 }
-.middle{
-    text-align: center;
+.middle {
+  text-align: center;
 }
-.loginout{
-    line-height: 60px;
-    text-decoration: none;
-    color: #000;
+.loginout {
+  line-height: 60px;
+  text-decoration: none;
+  color: #000;
 }
 </style>
