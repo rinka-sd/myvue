00fab45fe740f977dfd71eadabd10dde1bf17772
rinka-sd <1152657584@qq.com>
2020-03-20 11:40:59 +0800

    import msg-notice

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
+import MyServerHttp from '@/plugins/http.js'
+
+// elementUi css
 import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/assets/css/reset.css'
 import router from './router'
 
-//ElementUi插件
+//适用vue插件
+Vue.use(MyServerHttp)
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
